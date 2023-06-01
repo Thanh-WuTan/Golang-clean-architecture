39c02fc068e0c8d6c319024a9147ab548c222105
thanh-wutan <wt.vathanh@gmail.com>
2023-06-02 00:04:04 +0700

    update login controller

--- api/controller/login_controller.go
+++ api/controller/login_controller.go
@@ -5,14 +5,34 @@ import (
 	"onlyfounds/domain"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginController struct {
 	LoginUsecase domain.LoginUsecase
 }
 
-func (sc *LoginController) Login(c *gin.Context) {
+func (lc *LoginController) Login(c *gin.Context) {
+	var request domain.LoginRequest
+	err := c.ShouldBind(&request)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, domain.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	user, err := lc.LoginUsecase.GetUserByEmail(c, request.Email)
+	if err != nil {
+		c.JSON(http.StatusNotFound, domain.ErrorResponse{Message: "User not found with the given email"})
+		return
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Password)) != nil {
+		c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "Invalid credentials"})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"msg": "hello",
+		"user": user.Email,
 	})
 }
