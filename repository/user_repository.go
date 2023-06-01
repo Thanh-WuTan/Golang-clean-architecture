ec3415ee14bc12d1e67a72dfb7cfd0d734f03b02
thanh-wutan <wt.vathanh@gmail.com>
2023-06-01 23:53:33 +0700

    add GetByEmail method

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -19,3 +19,9 @@ func (ur *userRepository) Create(c context.Context, user *domain.User) error {
 	result := ur.database.Create(&user)
 	return result.Error
 }
+
+func (ur *userRepository) GetByEmail(c context.Context, email string) (domain.User, error) {
+	var user domain.User
+	result := ur.database.First(&user, "email = ?", email)
+	return user, result.Error
+}
